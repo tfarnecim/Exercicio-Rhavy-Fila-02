a0ccdb0f3556a05f6cf5b1e50180612b0ae0757c
tfarnecim <36956453+tfarnecim@users.noreply.github.com>
2018-12-20 14:37:04 +0000

    Update 1.py

--- Questão 1/1.py
+++ Questão 1/1.py
@@ -2,18 +2,18 @@ def empilhar(pilha,v):
     pilha.append(v)
     return pilha
 
-def desempilhar(pilha):
-    if(len(pilha)==0):
-        return pilha
-    pilha.pop(0)
-    return pilha
+def desenfileirar(fila):
+    if(len(fila)==0):
+        return fila
+    fila.pop(0)
+    return fila
 
-lista = [1,2,3]
+fila = [1,2,3]
 pilha = []
 
-for i in range(len(lista)):
-    pilha.append(lista[0])
-    lista.pop(0)
+for i in range(len(fila)):
+    pilha = empilhar(pilha,fila[0])
+    fila = desenfileirar(fila)
     
 for i in range(len(pilha)-1,-1,-1):
     print("%d"%(pilha[i]),end=' ')
